72d0fd5a408469b0d40d72f40100321ff66c41fe
m22r <h.matsuura116@gmail.com>
2021-05-09 22:55:25 +0900

    Add test ecs

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,12 +1,37 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     
-    new s3.Bucket(this, 'mys3bucket', {
-      versioned: true
+    const bucket = new s3.Bucket(this, 'mybucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    const cluster = new ecs.Cluster(this, 'mycluster', {});
+
+    const loadBalancedFargateService= new ecsPatterns.ApplicationMultipleTargetGroupsFargateService(this, 'myservice', {
+      cluster,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+      targetGroups: [
+        {
+          containerPort: 80,
+        },
+        {
+          containerPort: 90,
+          pathPattern: 'a/b/c',
+          priority: 10
+        }
+      ]
     });
   }
 }
